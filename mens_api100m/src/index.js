aeb13e231391c595dcea361961ada7539c27576c
Junaid <junaidhayat33303@gmail.com>
2023-12-07 19:07:46 +0500

    router commit

--- mens_api100m/src/index.js
+++ mens_api100m/src/index.js
@@ -1,60 +1,12 @@
 const express = require("express");
+const router = require("./routers/menrouter");
 require('./db/conn');
-const MenModel = require('./models/mens')
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
-// create mens route
-app.post('/men', async (req, res) => {
-    try {
-        const record = await new MenModel(req.body);
-        const result = await record.save();
-        res.status(201).send(result);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
-// get mens route
-app.get('/men', async (req, res) => {
-    try {
-        const record = await MenModel.find({})
-        res.status(200).send(record);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
-// get individual mens route
-app.get('/men/:id', async (req, res) => {
-    try {
-        const _id = req.params.id;
-        const record = await MenModel.find({ _id }).sort({ "ranking": 1 });
-        res.status(200).send(record);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
-
-// update  mens route
-app.patch('/men/:id', async (req, res) => {
-    try {
-        const _id = req.params.id;
-        const record = await MenModel.findByIdAndUpdate({ _id }, req.body, { new: true })
-        res.status(200).send(record);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-// delete  mens route
-app.delete('/men/:id', async (req, res) => {
-    try {
-        const _id = req.params.id;
-        const record = await MenModel.findByIdAndDelete({ _id })
-        res.status(200).send(record);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+app.use(router)
 
 app.listen(port, () => {
     console.log(`Server on port: ${port}`)
--- mens_api100m/src/routers/menrouter.js
+++ mens_api100m/src/routers/menrouter.js
@@ -1,1 +1,57 @@
 const express = require('express');
+const MenModel = require('../models/mens');
+
+const router = new express.Router();
+
+// create mens route
+router.post('/men', async (req, res) => {
+    try {
+        const record = await new MenModel(req.body);
+        const result = await record.save();
+        res.status(201).send(result);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+// get mens route
+router.get('/men', async (req, res) => {
+    try {
+        const record = await MenModel.find({})
+        res.status(200).send(record);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+// get individual mens route
+router.get('/men/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const record = await MenModel.find({ _id }).sort({ "ranking": 1 });
+        res.status(200).send(record);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// update  mens route
+router.patch('/men/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const record = await MenModel.findByIdAndUpdate({ _id }, req.body, { new: true })
+        res.status(200).send(record);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+// delete  mens route
+router.delete('/men/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const record = await MenModel.findByIdAndDelete({ _id })
+        res.status(200).send(record);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+module.exports = router;
