50638c3f6fdea219b8b2cc52711c9ae3f982f4d9
Junaid <junaidhayat33303@gmail.com>
2023-12-11 21:08:38 +0500

    another commit

--- mernbackend/src/index.js
+++ mernbackend/src/index.js
@@ -1,15 +1,26 @@
 const express = require("express")
 const path = require("path");
-require('./db/conn')
-const router = require("./routers/mernrouter");
+const hbs = require("hbs");
+require('./db/conn');
 const port = process.env.PORT || 3000;
 const app = express();
 
 const static_path = path.join(__dirname, "../public")
+const template_path = path.join(__dirname, "../templates/views")
+const partials_path = path.join(__dirname, "../templates/partials")
+
 app.use(express.static(static_path));
 
+app.set("view engine", "hbs")
+app.set("views", template_path);
+hbs.registerPartials(partials_path)
+
+
 app.use(express.json())
-app.use(router)
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
 
 
 app.listen(port, () => {
