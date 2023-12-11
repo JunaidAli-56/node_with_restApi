f11b4ad8ea962d8f3924ae525f6dc8994ef89fa2
Junaid <junaidhayat33303@gmail.com>
2023-12-12 00:47:37 +0500

    another commit

--- mernbackend/src/index.js
+++ mernbackend/src/index.js
@@ -21,6 +21,12 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.render("index")
 })
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+app.get("/login", (req, res) => {
+    res.render("login")
+})
 
 
 app.listen(port, () => {
